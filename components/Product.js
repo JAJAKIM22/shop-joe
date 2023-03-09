f7c4b43346d522fc53a5a361ce823c92bea08498
Peter <peterjajakim@gmail.com>
2023-03-09 11:54:56 +0300

    SHOP PAGE

---
+++ components/Product.js
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import { StarIcon } from "@heroicons/react/solid"
+import Currency from "react-currency-formatter"
+
+function Product({id, title, price, description, category, image}) {
+  return (
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
+      <Image src={image} height={200} width={200}  style={{objectFit:'contain'}} alt='new'/>
+      <h4>{title}</h4>
+      <div>
+      <StarIcon className="h-4 text-yellow-500"/>
+      </div>
+      <p className="text-sm my-2 line-clamp-2">{description}</p>
+      <div>
+       <Currency quantity={price} currency='KES' />
+      </div>
+      <button className="mt-auto button">Add to Basket</button>
+    </div>
+  )
+}
+
+export default Product
